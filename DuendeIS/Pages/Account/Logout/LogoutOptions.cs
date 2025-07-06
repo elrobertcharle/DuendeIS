c9096f216b99ee5ff63526c276f7db1e3adc1a48
Roberto C. Castaño <elrobertcharle@outlook.com>
2025-07-06 02:47:32 -0400

    added UI (dotnet new isui)

---
+++ DuendeIS/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,7 @@
+namespace DuendeIS.Pages.Logout;
+
+public static class LogoutOptions
+{
+    public static readonly bool ShowLogoutPrompt = true;
+    public static readonly bool AutomaticRedirectAfterSignOut = false;
+}
