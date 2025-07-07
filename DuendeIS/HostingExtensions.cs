e63ef6e61eec3c3198bc30f86dbfd3c3da9e5844
Roberto C. Castaño <elrobertcharle@outlook.com>
2025-07-07 04:52:43 -0400

    moved from in-memory to Psql both config and session data

--- DuendeIS/HostingExtensions.cs
+++ DuendeIS/HostingExtensions.cs
@@ -1,3 +1,7 @@
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
+using DuendeIS.Options;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 namespace DuendeIS;
@@ -9,12 +13,26 @@ internal static class HostingExtensions
         // uncomment if you want to add a UI
         builder.Services.AddRazorPages();
 
+        var migrationsAssembly = typeof(Program).Assembly.GetName().Name;
+        var postgresOptions = new PostgresDbOptions();
+        builder.Configuration.GetSection("Db").Bind(postgresOptions);
+        var connectionString = PostgresDbOptions.GetConnectionString(postgresOptions);
         builder.Services.AddIdentityServer()
-            .AddInMemoryIdentityResources(Config.IdentityResources)
-            .AddInMemoryApiScopes(Config.ApiScopes)
-            .AddInMemoryClients(Config.Clients)
-            .AddTestUsers(TestUsers.Users)
-            .AddLicenseSummary();
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseNpgsql(connectionString, b =>
+                {
+                    b.MigrationsAssembly(migrationsAssembly);
+                });
+            })
+        .AddOperationalStore(options =>
+        {
+            options.ConfigureDbContext = b => b.UseNpgsql(connectionString, b =>
+            {
+                b.MigrationsAssembly(migrationsAssembly);
+            });
+        }).AddTestUsers(TestUsers.Users)
+        .AddLicenseSummary();
 
         return builder.Build();
     }
@@ -28,6 +46,8 @@ internal static class HostingExtensions
             app.UseDeveloperExceptionPage();
         }
 
+        InitializeDatabase(app);
+
         // uncomment if you want to add a UI
         app.UseStaticFiles();
         app.UseRouting();
@@ -39,5 +59,50 @@ internal static class HostingExtensions
         app.MapRazorPages().RequireAuthorization();
 
         return app;
+    }
+
+    /// <summary>
+    /// dotnet ef migrations add InitialIdentityServerPersistedGrantDbMigration -c PersistedGrantDbContext -o Data/Migrations/IdentityServer/PersistedGrantDb
+    /// dotnet ef migrations add InitialIdentityServerConfigurationDbMigration -c ConfigurationDbContext -o Data/Migrations/IdentityServer/ConfigurationDb
+    /// 
+    /// dotnet ef database update -c PersistedGrantDbContext
+    /// dotnet ef database update -c ConfigurationDbContext
+    /// </summary>
+    /// <param name="app"></param>
+    private static void InitializeDatabase(IApplicationBuilder app)
+    {
+        using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope())
+        {
+            //serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+            var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+            //context.Database.Migrate();
+            if (!context.Clients.Any())
+            {
+                foreach (var client in Config.Clients)
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                foreach (var resource in Config.IdentityResources)
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            if (!context.ApiScopes.Any())
+            {
+                foreach (var resource in Config.ApiScopes)
+                {
+                    context.ApiScopes.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+        }
     }
 }
